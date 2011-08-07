274879dec4e642f9af690c5a06b0a6257ab78a7f
= <jcaldwell@phase2technology.com>
2011-08-07 15:43:35 -0400

    updated version

--- docReady.js
+++ docReady.js
@@ -16,56 +16,59 @@ var bind = function(o, f){
 };
 
 function DocReady(func){
-	this.init();
-	if(func) this.add(func);
+    this.init();
+    if(func) this.add(func);
 }
 
-DocReady.prototype.init = function(){
-	this.ran = false;
-	//ensure that we have soemthing to bind
-	if(typeof(this.readyFunctions) != 'function'){
-	  this.readyFunctions = function(){};
-	}
-		
-	var runFunc = bind(this, this.readyFunctions);
+DocReady.prototype.readyFunctions = function() { };
 
-	if(document.addEventListener){ //Moz or Opera
-		document.addEventListener('DOMContentLoaded', runFunc);
-		window.addEventListener('load', runFunc); //just in case
-	} else if(document.all && !window.opera && document.readyState) { //IE
-		var src = (window.location.protocol == 'https:') ? '://0' : 'javascript:void(0)';
-		document.write("<script id='DOMReady' defer=true src='" + src + "'><\/script>");  
-		document.getElementById("DOMReady").onreadystatechange=function(){
-		    if (this.readyState=="complete"){ runFunc(); }
-		};
-	} else if(document.readyState && (navigator.userAgent.indexOf('AppleWebKit/') > -1)){ //safari
-		this.timer = setInterval(function() {
-			if (document.readyState == 'loaded' || document.readyState == 'complete') { runFunc(); }
-		}, 50);
-	} else { //older browsers
-		var fn = window.onload;
-		window.onload = function() {
-			runFunc();
-			if (fn) fn();
-		};
-	}
-};
+DocReady.prototype.init = function(){
+  //flag
+  this.ran = false;
+  this.readyFunctions = new Array();
+  this.addListeners(this.readyFunctions);
+}
+
+DocReady.prototype.addListeners = function(funcs)
+{
+    var runFunc = bind(this, function(){
+    if(!this.ran){
+      for(var i = 0; i < funcs.length; i++){
+        if(typeof(funcs[i]) == 'function'){
+          (funcs[i])();
+        }
+      }
+      this.ran = true;
+    }
+  });
+    //register event Readyrs
+    if(document.addEventListener){ //Moz or Opera
+        document.addEventListener('DOMContentLoaded', runFunc, false);
+        window.addEventListener('load', runFunc, false); //just in case
+    } else if(document.all && !window.opera && document.readyState) { //IE
+        var src = (window.location.protocol == 'https:') ? '://0' : 'javascript:void(0)';
+        document.write("<script id='DOMReady' defer=true src='" + src + "'><\/script>");  
+        document.getElementById("DOMReady").onreadystatechange=function(){
+            if (this.readyState=="complete"){ runFunc(); }
+        }
+    } else if(document.readyState && (navigator.userAgent.indexOf('AppleWebKit/') > -1)){ //safari
+        this.timer = setInterval(function() {
+            if (document.readyState == 'loaded' || document.readyState == 'complete') { runFunc(); }
+        }, 50);
+    } else { //older browsers
+        var fn = window.onload;
+        window.onload = function() {
+            runFunc();
+            if (fn) fn();
+        }
+    }
+}
 
 DocReady.prototype.add = function(func){
-	if(typeof(func) != 'function'){ return false; }
-	if(this.ran){ return func(); }
-	var fn;
-	if(typeof(this.readyFunctions) == 'function')
-		fn = bind(this, this.readyFunctions);
-	else
-		fn = function(){};	
-	this.readyFunctions=function(){
-		if(!this.ran){
-			if(this.timer){ clearInterval(this.timer); }
-			fn();	func();
-		}
-	}
-};
-
-//create an instance
+  if(typeof(func) != 'function'){ return false; }
+  if(this.ran){ return func(); }
+  this.readyFunctions.push(func);
+  this.addListeners(this.readyFunctions);
+}
+//Create a DocReady object to register functions with
 window.dR = new DocReady();
